da5524af484a4f7b966c9d0c0c8289d2374c8597
circledflow-maker <circle.d.flow@gmail.com>
2026-02-10 19:31:51 +0000

    Add Netlify checkout function

---
+++ netlify/functions/create-checkout.js
@@ -0,0 +1,25 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  try {
+    const { priceId } = JSON.parse(event.body);
+
+    const session = await stripe.checkout.sessions.create({
+      mode: 'payment',
+      payment_method_types: ['card'],
+      line_items: [{ price: priceId, quantity: 1 }],
+      success_url: `${process.env.URL}/success.html`,
+      cancel_url: `${process.env.URL}/pages/marketplace.html`,
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ id: session.id }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
